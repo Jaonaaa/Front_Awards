85909fa02fc71fc1bd95a2976016ea56b0d09de1
Jaonaaa <jaonapaulferdinah@gmail.com>
2023-09-19 11:01:21 +0300

    animate head section

--- src/components/Section/Section.js
+++ src/components/Section/Section.js
@@ -1,4 +1,4 @@
-import { initProps, props, getStyle } from "../../utils/index.js";
+import { initProps, props, getStyle, wait } from "../../utils/index.js";
 
 /**
  * @param {props} props
@@ -21,22 +21,57 @@ export function Section_Horiz(props) {
 
 function handleSectionProps(section, props) {
   if (props.titleOn) {
-    let head = section_title({
+    let head = section_head({
       title: props.titleOn.title,
       subtitle: props.titleOn.subtitle,
     });
     section.appendChild(head);
   }
 }
-function section_title({ title, subtitle }) {
+function section_head({ title, subtitle }) {
   const head = document.createElement("div");
   head.classList.add("section_head");
 
   head.innerHTML = `
-  <div class="title"> ${title} </div>
+  <div class="title hide"> ${title} </div>
 
-  <div class="subtitle"> ${subtitle} </div>
+  <div class="subtitle hide"> ${subtitle} </div>
   `;
-
+  setUpSection_head(head);
   return head;
+}
+
+/**
+ *
+ * @param {HTMLElement} container
+ */
+function setUpSection_head(container) {
+  let observer = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          console.log;
+          animation_head(entry.target);
+          observer.unobserve(entry.target);
+        }
+      });
+    },
+    {
+      threshold: 0.9,
+    }
+  );
+  observer.observe(container);
+}
+/**
+ *
+ * @param {HTMLElement} head
+ */
+function animation_head(head) {
+  let timer = 100;
+  Array.from(head.children).forEach((child) => {
+    wait(() => {
+      child.classList.remove("hide");
+    }, timer);
+    timer += 350;
+  });
 }
